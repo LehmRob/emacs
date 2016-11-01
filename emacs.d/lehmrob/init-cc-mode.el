ebbb2c2a39bfa98b2c6c2e91506bffabfabb0d86
Robert Lehmann <robert.lehmann@sitec-systems.de>
2016-11-01 08:53:03 +0100

    Update modes

---
+++ emacs.d/lehmrob/init-cc-mode.el
@@ -0,0 +1,16 @@
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(c-add-style "lehmrob-style"
+			 '("stroustrup"
+			   (c-basic-offset . 2)
+			   (c-set-offset 'substatement-open '0)
+			   (c-set-offset 'case-label '+)
+			   (c-set-offset 'innamespace '0)))
+
+(defun my-cpp-mode-hook()
+  (c-set-style "lehmrob-style")
+  (auto-fill-mode)
+  (c-toggle-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-cpp-mode-hook)
+
+(provide 'init-cc-mode)
