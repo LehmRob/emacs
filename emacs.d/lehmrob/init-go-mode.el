87be3cb63798e4a895e10cf516dca5517b5f367c
Robert Lehmann <lehmrob@gmail.com>
2017-02-05 19:23:25 +0100

    Update dotfiles

--- emacs.d/lehmrob/init-go-mode.el
+++ emacs.d/lehmrob/init-go-mode.el
@@ -1,13 +1,11 @@
+(maybe-require-package 'go-mode)
+
 (defun my-go-mode-hook ()
-  ; Use goimports instead of go-fmt
-  (setq gofmt-command "/home/rd7/work/go/bin/goimports")
-  ; Call Gofmt before saving
+  (setq gofmt-command (concat (getenv "GOPATH") ,"/bin/goimports"))
   (add-hook 'before-save-hook 'gofmt-before-save)
-  ; Customize compile command to run go build
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet"))
-  ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (provide 'init-go-mode)
